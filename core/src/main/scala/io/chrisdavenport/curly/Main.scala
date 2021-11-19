a9f2057472f40a5ae7812c6312c196d2a561ae01
Christopher Davenport <chris@christopherdavenport.tech>
2021-11-18 17:50:41 -0800

    Push current code

--- core/src/main/scala/io/chrisdavenport/curly/Main.scala
+++ core/src/main/scala/io/chrisdavenport/curly/Main.scala
@@ -6,9 +6,15 @@ import cats.syntax.all._
 object Main extends IOApp {
 
   def run(args: List[String]): IO[ExitCode] = {
-    // val x = "curl 'https://linux.die.net/man/1/curl' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: https://www.google.com/' -H 'Connection: keep-alive' -H 'Cookie: u=0ZfsEl8jjh6BR0aoAw9hAg==' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: cross-site' -H 'Sec-Fetch-User: ?1' -H 'Cache-Control: max-age=0'"
-    val x = "curl 'https://localhost:8080/' --http2"
-    val p = Curly.all.parse(x)
+    val x = "curl 'https://linux.die.net/man/1/curl' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: https://www.google.com/' -H 'Connection: keep-alive' -H 'Cookie: u=0ZfsEl8jjh6BR0aoAw9hAg==' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: cross-site' -H 'Sec-Fetch-User: ?1' -H 'Cache-Control: max-age=0'"
+    // val x = "curl -H 'foo' 'https://localhost:8080/'"
+    // val p = Curly.opts.parse("--header 'foo'")
+    // val opt = Curly.OptC('H'.some, "header", true)
+    // val i = "'https://localhost:8080/'"
+    // val p = Curly.opts.parse("-H 'foo'")
+    // val p = Curly.uri.parse(i)
+    // val p = (Curly.curl *> Curly.uri ~ Curly.opts).parse(x)
+    val p = Curly.all.parseAll(x)
 
     IO(println(p)).as(ExitCode.Success)
   }
@@ -19,9 +25,10 @@ object Main extends IOApp {
 object Curly {
   sealed trait Component
   case class Uri(s: String) extends Component
-  case class Method(x: String) extends Component
-  case class Header(key: String, value: String) extends Component
-  case class Unknown(option: String, value: Option[String]) extends Component
+  // case class Method(x: String) extends Component
+  // case class Header(key: String, value: String) extends Component
+  case class Opt(option: String, value: Option[String]) extends Component
+  case class Unhandled(value: String) extends Component
 
   import cats.parse.Rfc5234.{alpha, sp}
   import cats.parse.Rfc5234
@@ -32,33 +39,85 @@ object Curly {
 
   val uri = P.char('\'') *> P.until(P.char('\'')) <* P.char('\'') <* sp.?
 
-  val method = {
-    (P.string("-X ") | P.string("--request ")) *>
-      P.until(sp) <* sp.?
-  }.map(Method(_))
-  
-  val header = {
-    val h = P.string("-H ") | P.string("--header ")
-    val key = P.char('\'') *> P.until(P.char(':')) <* P.char(':') <* sp.?
-    val value = P.until(P.char('\'')) <* P.char('\'') <* sp.?
-    h *> (key ~ value).map{ case (a, b) => Header(a, b)}
+  case class OptC(short: Option[Char], long: String, hasValue: Boolean)
+
+  val optConfigs: List[OptC] = List(
+    OptC('a'.some, "append", false),
+    OptC('A'.some, "user-agent", true),
+    OptC(None, "anyauth", false),
+    OptC('b'.some, "cookie", true),
+    OptC('B'.some, "use-ascii", false),
+    OptC(None, "basic", false),
+    OptC(None, "ciphers", true),
+    OptC(None, "connect-timeout", true),
+    OptC('c'.some, "cookie-jar", true),
+    OptC('C'.some, "continue-at", true),
+    OptC(None, "create-dirs", false),
+    OptC(None, "crlf", false),
+    OptC(None, "crlfile", false),
+    OptC('d'.some, "data", true),
+    OptC(None, "data-binary", true),
+    OptC(None, "data-urlencode", true),
+    OptC(None, "digest", false),
+    OptC(None, "disable-eprt", false),
+    OptC(None, "disable-epsv", false),
+    OptC('D'.some, "dump-header", true),
+    OptC('e'.some, "referer", true),
+    OptC(None, "engine", true),
+    OptC(None, "environment", false),
+    OptC(None, "egd-file", true),
+    OptC('E'.some, "cert", true),
+    OptC(None, "cert-type", true),
+    OptC(None, "ca-cert", true),
+    OptC(None, "capath", true),
+    OptC('f'.some, "fail", false),
+    OptC(None, "ftp-account", false),
+    OptC(None, "ftp-method", true),
+    OptC(None, "ftp-pasv", false),
+    OptC(None, "ftp-alternative-to-user", true),
+    OptC(None, "ftp-skip-pasv-ip", false),
+    OptC(None, "ftp-ssl", false),
+    OptC(None, "ftp-ssl-control", false),
+    OptC(None, "ftp-ssl-reqd", false),
+    OptC(None, "ftp-ssl-ccc", false),
+    OptC(None, "ftp-ssl-ccc-mode", true),
+    OptC('F'.some, "form", true),
+    OptC('H'.some, "header", true),
+    OptC(None, "http2", false),
+
+    OptC('X'.some, "request", true)
+  )
+
+  def known: P[Opt] = {
+    optConfigs.foldRight[P[Opt]](P.fail){ case (optC, p) => parseOptC(optC).backtrack | p}
   }
 
-  val unknown: P[Unknown] = {
-    val optDouble = P.string("--") *> (P.not(P.end | sp).with1 ~ P.anyChar).rep.string <* sp.?
-    val optSingle = P.string("-") *> P.until(sp) <* sp.?
-    val opt = optDouble | optSingle
-    val value: P0[Option[String]] = 
-      P.peek(P.char('-')).as(Option.empty[String]) |
-      (P.until(sp) <* sp.?).map(s => if (s.isEmpty()) None else Some(s))
-    (opt ~ value).map{ case (a,b) => Unknown(a, b)}
+  def parseOptC(optC: OptC): P[Opt] = {
+    val short = optC.short match{
+      case Some(c) => P.string("-") *> P.char(c) <* sp.?.void
+      case None => P.fail
+    }
+    val long = P.string("--") *> P.string(optC.long) <* sp.?.void
+    val code = short | long
+    val body = if (optC.hasValue) {
+      P.char('$').? *> P.char('\'') *> P.until(P.char('\'')).map(_.some) <* P.char('\'') <* sp.? |
+      P.until(sp).map(_.some) <* sp.? 
+    } else P.unit.as(None)
+    (code ~ body).map{
+      case (_, value) => Opt(optC.long, value)
+    }
   }
 
-  val opts: P[Component] = method  | header | unknown
+  val unhandled: P[Unhandled] = (
+    P.char('$').?.with1 *> P.char('\'') *> P.until(P.char('\'')) <* P.char('\'') <* sp.? |
+    P.until(sp) <* sp.? 
+  ).map(Unhandled(_))
+
+
+  val opts: P[Component] = known | unhandled //| method  | header | unknown
 
   val all = curl *>
-    (uri ~
-      unknown
-    )
+    opts.rep0
+
 
 }
