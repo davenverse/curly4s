b9ec066a4f1162ac774aec9c83d97f10fbdc312b
Andrew Valencik <andrew.valencik@gmail.com>
2021-11-18 23:16:08 -0500

    Rewrite tests to assert the shape

--- core/src/test/scala/io/chrisdavenport/curly/MainSpec.scala
+++ core/src/test/scala/io/chrisdavenport/curly/MainSpec.scala
@@ -3,43 +3,53 @@ package io.chrisdavenport.curly
 
 class MainSpec extends munit.FunSuite {
   import CurlyParser._
+  import Opts._
 
   test("leading 'curl' is case insensitive") {
     val cmd = "CuRl 'https://typelevel.org/'"
     val actual = CurlyParser.all.parseAll(cmd)
-    // TODO actually a URI
-    val expected = List(Opts.Unhandled("https://typelevel.org/"))
+    val expected = List(Unhandled("https://typelevel.org/"))
     assertEquals(actual, Right(expected))
   }
 
   test("only a URL") {
     val cmd = "curl 'https://typelevel.org/'"
     val actual = CurlyParser.all.parseAll(cmd)
-    // TODO actually a URI
-    val expected = List(Opts.Unhandled("https://typelevel.org/"))
+    val expected = List(Unhandled("https://typelevel.org/"))
     assertEquals(actual, Right(expected))
   }
-
-  //
-  // Just asserting lengths for now...
-  //
 
   test("with one header") {
     val cmd = "curl 'https://typelevel.org/' -H 'User-Agent: Mozilla/5.0'"
     val actual = CurlyParser.all.parseAll(cmd)
-    assertEquals(actual.map(_.length), Right(2))
+    val expected = List(
+      Unhandled("https://typelevel.org/"),
+      Opt("header", "User-Agent: Mozilla/5.0"),
+    )
+    assertEquals(actual, Right(expected))
   }
 
   test("with two headers") {
     val cmd = "curl 'https://typelevel.org/' -H 'User-Agent: Mozilla/5.0' -H 'Connection: keep-alive'"
     val actual = CurlyParser.all.parseAll(cmd)
-    assertEquals(actual.map(_.length), Right(3))
+    val expected = List(
+      Unhandled("https://typelevel.org/"),
+      Opt("header", "User-Agent: Mozilla/5.0"),
+      Opt("header", "Connection: keep-alive"),
+    )
+    assertEquals(actual, Right(expected))
   }
 
-  test("with two headers separated by a long arg") {
+  test("with two headers separated by a long arg".fail) {
     val cmd = "curl 'https://typelevel.org/' -H 'User-Agent: Mozilla/5.0' --compress -H 'Connection: keep-alive'"
     val actual = CurlyParser.all.parseAll(cmd)
-    assertEquals(actual.map(_.length), Right(4))
+    val expected = List(
+      Unhandled("https://typelevel.org/"),
+      Opt("header", "User-Agent: Mozilla/5.0"),
+      Flag("compress"),
+      Opt("header", "Connection: keep-alive"),
+    )
+    assertEquals(actual, Right(expected))
   }
 
 }
